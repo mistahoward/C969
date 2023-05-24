feccaa99660cd76018cff013cbe16eaa0b4c873b
mistahoward <me@alexhoward.dev>
2023-05-23 23:33:51 -0500

    updating git ignore, starting project up, etc etc

---
+++ C969/LoginForm.Designer.cs
@@ -0,0 +1,134 @@
+﻿
+namespace C969
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.UsernameTextBox = new System.Windows.Forms.TextBox();
+            this.PasswordTextBox = new System.Windows.Forms.TextBox();
+            this.UsernameLabel = new System.Windows.Forms.Label();
+            this.PasswordLabel = new System.Windows.Forms.Label();
+            this.errorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            this.loginButton = new System.Windows.Forms.Button();
+            this.CloseButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // UsernameTextBox
+            // 
+            this.UsernameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F);
+            this.UsernameTextBox.Location = new System.Drawing.Point(12, 29);
+            this.UsernameTextBox.Name = "UsernameTextBox";
+            this.UsernameTextBox.Size = new System.Drawing.Size(217, 22);
+            this.UsernameTextBox.TabIndex = 0;
+            // 
+            // PasswordTextBox
+            // 
+            this.PasswordTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F);
+            this.PasswordTextBox.Location = new System.Drawing.Point(12, 93);
+            this.PasswordTextBox.Name = "PasswordTextBox";
+            this.PasswordTextBox.Size = new System.Drawing.Size(217, 22);
+            this.PasswordTextBox.TabIndex = 1;
+            // 
+            // UsernameLabel
+            // 
+            this.UsernameLabel.AutoSize = true;
+            this.UsernameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F);
+            this.UsernameLabel.Location = new System.Drawing.Point(12, 9);
+            this.UsernameLabel.Name = "UsernameLabel";
+            this.UsernameLabel.Size = new System.Drawing.Size(73, 17);
+            this.UsernameLabel.TabIndex = 2;
+            this.UsernameLabel.Text = "Username";
+            // 
+            // PasswordLabel
+            // 
+            this.PasswordLabel.AutoSize = true;
+            this.PasswordLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F);
+            this.PasswordLabel.Location = new System.Drawing.Point(12, 73);
+            this.PasswordLabel.Name = "PasswordLabel";
+            this.PasswordLabel.Size = new System.Drawing.Size(69, 17);
+            this.PasswordLabel.TabIndex = 3;
+            this.PasswordLabel.Text = "Password";
+            // 
+            // errorProvider
+            // 
+            this.errorProvider.ContainerControl = this;
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(169, 157);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(75, 23);
+            this.loginButton.TabIndex = 4;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.LoginButton_Click);
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.Location = new System.Drawing.Point(12, 157);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(75, 23);
+            this.CloseButton.TabIndex = 5;
+            this.CloseButton.Text = "Close";
+            this.CloseButton.UseVisualStyleBackColor = true;
+            this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(256, 194);
+            this.Controls.Add(this.CloseButton);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.PasswordLabel);
+            this.Controls.Add(this.UsernameLabel);
+            this.Controls.Add(this.PasswordTextBox);
+            this.Controls.Add(this.UsernameTextBox);
+            this.Name = "LoginForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.LoginForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox UsernameTextBox;
+        private System.Windows.Forms.TextBox PasswordTextBox;
+        private System.Windows.Forms.Label UsernameLabel;
+        private System.Windows.Forms.Label PasswordLabel;
+        private System.Windows.Forms.ErrorProvider errorProvider;
+        private System.Windows.Forms.Button CloseButton;
+        private System.Windows.Forms.Button loginButton;
+    }
+}
+
---
+++ C969/LoginForm.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C969
+{
+    public partial class LoginForm : Form
+    {
+        Dictionary<string, Dictionary<string, string>> translations = new Dictionary<string, Dictionary<string, string>>
+    {
+        { "en-US", new Dictionary<string, string> {
+            {"login", "Login"},
+            {"username", "Username"},
+            {"password", "Password"},
+            {"username_error", "Username cannot be empty."},
+            {"password_error", "Password cannot be empty." },
+            {"error", "The username and password did not match."}
+        }},
+        { "fr-FR", new Dictionary<string, string> {
+            {"login", "S'identifier"},
+            {"username", "Nom d'utilisateur"},
+            {"password", "Mot de passe"},
+            {"username_error", "Le nom d'utilisateur ne peut pas être vide." },
+            {"password_error", "Le mot de passe ne peut pas être vide." },
+            {"error", "Le nom d'utilisateur et le mot de passe ne correspondent pas."}
+        }}
+    };
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+        // Helper function for getting language (locale code)
+        private string GetLanguage()
+        {
+            string localeCode = System.Globalization.CultureInfo.CurrentUICulture.IetfLanguageTag;
+
+            if (translations.ContainsKey(localeCode))
+            {
+                return localeCode;
+            }
+            else
+            {
+                // Default to English if the system language isn't one of the hardcoded options.
+                return "en-US";
+            }
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+            // grab selected language
+            string selectedLanguage = this.GetLanguage();
+
+            // choose selected language translations
+            UsernameLabel.Text = translations[selectedLanguage]["username"];
+            PasswordLabel.Text = translations[selectedLanguage]["password"];
+        }
+        private void LoginButton_Click(object sender, EventArgs e)
+        {
+            // clear any existing errors first and foremost
+            errorProvider.Clear();
+
+            string selectedLanguage = this.GetLanguage();
+            // begin error checking, check if fields are empty or whitespace
+            if(string.IsNullOrWhiteSpace(UsernameTextBox.Text))
+            {
+                errorProvider.SetError(UsernameTextBox, translations[selectedLanguage]["username_error"]);
+            }
+            if (string.IsNullOrWhiteSpace(PasswordTextBox.Text)) {
+                errorProvider.SetError(PasswordTextBox, translations[selectedLanguage]["password_error"]);
+            }
+        }
+
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+    }
+}
