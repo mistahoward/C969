a50b3c756ad63468322cfe6a0ef9057c7559fdbe
mistahoward <me@alexhoward.dev>
2023-06-01 11:37:54 -0500

    adding get user by id method

--- C969/Data/UserData.cs
+++ C969/Data/UserData.cs
@@ -1,4 +1,5 @@
 ﻿using C969.Models;
+using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +12,33 @@ namespace C969.Data
     {
         public User GetUserById(int id)
         {
-            // implementation goes here
+            User emptyUser = new User();
+            User resultUser = null;
+
+            using (var sqlResponse = RetrieveData(emptyUser, "id", id))
+            {
+                if (sqlResponse.Read())
+                {
+                    resultUser = new User
+                    {
+                        userId = sqlResponse.GetInt32(sqlResponse.GetOrdinal("userId")),
+                        userName = sqlResponse.GetString(sqlResponse.GetOrdinal("userName")),
+                        password = sqlResponse.GetString(sqlResponse.GetOrdinal("password")),
+                        active = sqlResponse.GetBoolean(sqlResponse.GetOrdinal("active")),
+                        createDate = sqlResponse.GetDateTime(sqlResponse.GetOrdinal("createDate")),
+                        createdBy = sqlResponse.GetString(sqlResponse.GetOrdinal("createdBy")),
+                        lastUpdate = sqlResponse.GetDateTime(sqlResponse.GetOrdinal("lastUpdate")),
+                        lastUpdateBy = sqlResponse.GetString(sqlResponse.GetOrdinal("lastUpdateBy"))
+                    };
+                }
+            }
+
+            if (resultUser == null)
+            {
+                throw new Exception("No user found with the provided ID.");
+            }
+
+            return resultUser;
         }
     }
 
