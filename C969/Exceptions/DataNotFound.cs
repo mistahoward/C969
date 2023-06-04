0d294b38b1dd3a37f47b025d058e89240c4d3c6e
mistahoward <me@alexhoward.dev>
2023-06-03 20:44:56 -0500

    Adding custom exception

---
+++ C969/Exceptions/DataNotFound.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C969.Exceptions
+{
+    public class DataNotFound : Exception
+    {
+        public DataNotFound() { }
+
+        public DataNotFound(string message) : base(message) { }
+
+        public DataNotFound(string message, Exception innerException) : base(
+            message, innerException)
+        { 
+        }
+    }
+}
