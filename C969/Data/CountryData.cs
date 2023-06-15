96e5fce47182bc53a81ed939f716518e4527efa0
mistahoward <me@alexhoward.dev>
2023-06-14 19:07:33 -0500

    adding get country by name

--- C969/Data/CountryData.cs
+++ C969/Data/CountryData.cs
@@ -3,6 +3,7 @@ using C969.Models;
 using C969.Utilities;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,24 @@ namespace C969.Data
 {
     public class CountryData : Database
     {
+        /// <summary>
+        /// Gets an country from the db by name
+        /// </summary>
+        /// <param name="countryName">Name of the Address (Address.address)</param>
+        /// <exception cref="ArgumentException">Thrown when country name is null or whitespace</exception>
+        /// <exception cref="DataNotFound">Thrown when country object is not found</exception>
+        public Country GetCountryByName(string countryName)
+        {
+            if (string.IsNullOrWhiteSpace(countryName))
+            {
+                throw new ArgumentException($"'{nameof(countryName)}' cannot be null or whitespace", nameof(countryName));
+            }
+            Country emptyCountry = new Country();
+            DataRow countryRow = RetrieveSingleRow(emptyCountry, "country", countryName);
+            Country resultCountry = DataTableConverter.ConvertDataRowToModel<Country>(countryRow)
+                ?? throw new DataNotFound("No country found with provided name");
+            return resultCountry;
+        }
         public bool AddCountry(Country workingCountry)
         {
             var validCountry = ModelValidator.ValidateModel(workingCountry);
