b5cc7e328db0ebfe077116fcf761359d1dc92f8e
mistahoward <me@alexhoward.dev>
2023-06-14 19:09:34 -0500

    adding getcountrybyid

--- C969/Data/CountryData.cs
+++ C969/Data/CountryData.cs
@@ -13,6 +13,25 @@ namespace C969.Data
     public class CountryData : Database
     {
         /// <summary>
+        /// Gets a country from the db by id
+        /// </summary>
+        /// <param name="id">ID of the country</param>
+        /// <returns>Country if success, exception if fail</returns>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when id is less than 0</exception>
+        /// <exception cref="DataNotFound">Thrown when country object is not found</exception>
+        public Country GetCountryById(int id)
+        {
+            if (id < 0)
+            {
+                throw new ArgumentOutOfRangeException("ID cannot be a number less than zero");
+            }
+            Country emptyCountry = new Country();
+            DataRow countryRow = RetrieveSingleRow(emptyCountry, "countryId", id);
+            Country resultCountry = DataTableConverter.ConvertDataRowToModel<Country>(countryRow)
+                ?? throw new DataNotFound("No  country found with the provided ID");
+            return resultCountry;
+        }
+        /// <summary>
         /// Gets an country from the db by name
         /// </summary>
         /// <param name="countryName">Name of the Address (Address.address)</param>
