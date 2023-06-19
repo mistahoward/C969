8ddfb4501f0926979da2efa442826e9492124fb0
mistahoward <me@alexhoward.dev>
2023-06-19 17:42:21 -0500

    Wait - readding to addressData. That check does not need to exist for objects with an active bool.

--- C969/Data/AddressData.cs
+++ C969/Data/AddressData.cs
@@ -189,6 +189,12 @@ namespace C969.Data
             }
 
             Address claimedAddress = GetAddressById(id);
+            bool existingAddress = DoesAddressExist(claimedAddress);
+
+            if (!existingAddress)
+            {
+                throw new DataNotFound("Address does not exist");
+            }
 
             return DeleteData<Address>($"addressId = {id}");
         }
