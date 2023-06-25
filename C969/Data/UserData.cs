d803009beabbd4a37b80d8cef4812d22d4d5d104
mistahoward <me@alexhoward.dev>
2023-06-24 21:53:12 -0500

    Oops... Forgot to add a login function. :)

--- C969/Data/UserData.cs
+++ C969/Data/UserData.cs
@@ -50,11 +50,35 @@ namespace C969.Data
             return resultUser;
         }
         /// <summary>
+        /// Verifies if a provided password matches the password of a user in the database.
+        /// </summary>
+        /// <param name="workingUser">The user object for which the password is to be verified</param>
+        /// <returns>True if the password is verified, false otherwise</returns>
+        /// <exception cref="InvalidObject">Thrown when user object is not valid or password is whitespace</exception>
+        public bool VerifyPassword(User workingUser)
+        {
+            var validUser = ModelValidator.ValidateModel(workingUser);
+            if (!validUser)
+            {
+                throw new InvalidObject("User isn't valid");
+            }
+            if (string.IsNullOrWhiteSpace(workingUser.password))
+            {
+                throw new InvalidObject("Password cannot be empty or whitespace");
+            }
+            var claimedUser = GetUserById(workingUser.userId);
+            if (claimedUser.password == workingUser.password)
+            {
+                return true;
+            }
+            return false;
+        }
+        /// <summary>
         /// Marks a user as inactive in db
         /// </summary>
-        /// <param name="id">userId to makr as inactive</param>
+        /// <param name="id">userId to mark as inactive</param>
         /// <returns>Boolean of success</returns>
-        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when id is less than 0</exception>
         public bool DeleteUserById(int id)
         {
             if (id < 0)
